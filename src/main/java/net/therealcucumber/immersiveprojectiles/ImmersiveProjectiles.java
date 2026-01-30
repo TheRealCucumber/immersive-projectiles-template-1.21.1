d22c1195493401bba16b34651039d0434e0253e1
TheRealCucumber <cucumber3425@gmail.com>
2026-01-29 19:07:35 -0500

    added ModItems file components

--- src/main/java/net/therealcucumber/immersiveprojectiles/ImmersiveProjectiles.java
+++ src/main/java/net/therealcucumber/immersiveprojectiles/ImmersiveProjectiles.java
@@ -2,6 +2,7 @@ package net.therealcucumber.immersiveprojectiles;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.therealcucumber.immersiveprojectiles.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +12,6 @@ public class ImmersiveProjectiles implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/net/therealcucumber/immersiveprojectiles/item/ModItems.java
+++ src/main/java/net/therealcucumber/immersiveprojectiles/item/ModItems.java
@@ -1,4 +1,20 @@
 package net.therealcucumber.immersiveprojectiles.item;
 
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.therealcucumber.immersiveprojectiles.ImmersiveProjectiles;
+
 public class ModItems {
+
+
+private static Item registerItem(String name, Item item){
+    return Registry.register(Registries.ITEM, Identifier.of(ImmersiveProjectiles.MOD_ID, name), item)
+}
+
+    public static void registerModItems() {
+        ImmersiveProjectiles.LOGGER.info("Registering Mod Items for " + ImmersiveProjectiles.MOD_ID);
+    }
+
 }
