21417cf1187302077751ae76d12f753162dacdf2
TheRealCucumber <cucumber3425@gmail.com>
2026-01-30 17:43:18 -0500

    added the pebble and iron shaft items and things that come with them

--- src/main/java/net/therealcucumber/immersiveprojectiles/item/ModItems.java
+++ src/main/java/net/therealcucumber/immersiveprojectiles/item/ModItems.java
@@ -1,20 +1,28 @@
 package net.therealcucumber.immersiveprojectiles.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.therealcucumber.immersiveprojectiles.ImmersiveProjectiles;
 
 public class ModItems {
+    public static final Item IRON_SHAFT = registerItem("iron_shaft", new Item(new Item.Settings()));
+    public static final Item PEBBLE = registerItem("pebble", new Item(new Item.Settings()));
 
-
-private static Item registerItem(String name, Item item){
-    return Registry.register(Registries.ITEM, Identifier.of(ImmersiveProjectiles.MOD_ID, name), item)
+private static Item registerItem(String name, Item item) {
+    return Registry.register(Registries.ITEM, Identifier.of(ImmersiveProjectiles.MOD_ID, name), item);
 }
 
     public static void registerModItems() {
         ImmersiveProjectiles.LOGGER.info("Registering Mod Items for " + ImmersiveProjectiles.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(IRON_SHAFT);
+        });
     }
 
 }
