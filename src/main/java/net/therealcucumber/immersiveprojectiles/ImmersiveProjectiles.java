c6eb54cc880dc7f896c778a1b389baa99d551582
TheRealCucumber <cucumber3425@gmail.com>
2026-01-31 23:26:38 -0500

    deleted ModItemsGroup java class due to it being problematic

--- src/main/java/net/therealcucumber/immersiveprojectiles/ImmersiveProjectiles.java
+++ src/main/java/net/therealcucumber/immersiveprojectiles/ImmersiveProjectiles.java
@@ -3,7 +3,6 @@ package net.therealcucumber.immersiveprojectiles;
 import assets.immersiveprojectiles.block.ModBlocks;
 import net.fabricmc.api.ModInitializer;
 
-import net.therealcucumber.immersiveprojectiles.item.ModItemGroups;
 import net.therealcucumber.immersiveprojectiles.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
